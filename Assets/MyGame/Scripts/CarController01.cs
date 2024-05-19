ae64e4cb7c278c90b6639355ed0c256b80380533
hoanghaisc001 <69134435+hoanghaisc001@users.noreply.github.com>
2024-05-19 08:27:47 +0700

    add
    add

---
+++ Assets/MyGame/Scripts/CarController01.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController01 : MonoBehaviour
+{
+   
+       [SerializeField] private float speedMove = 10f;
+       [SerializeField] private float speedRotate = 30f;
+       public float maxHealth = 100;
+       private float currentHealth = 0;
+       public float fuelValue = 20;
+       public float damageValue = 50;
+
+       public GameObject explusionPrefab;
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            transform.Translate(new Vector3(0, 0, vertical * speedMove * Time.deltaTime));
+            transform.Rotate(new Vector3(0, horizontal * speedRotate * Time.deltaTime, 0));
+            
+        }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fuel")
+        {
+            Destroy(other.gameObject);
+            GameManager.Instance.SetFuel(fuelValue);
+            InstantiateGame(other);
+        }
+        else if(other.tag == "Damage")
+        {
+            DamageHealth(damageValue);
+            Debug.LogWarning("So Health con lai : " + currentHealth);
+            InstantiateGame(other);
+            
+        }
+
+    }
+
+    void InstantiateGame(Collider other)
+    {
+        Instantiate(explusionPrefab, other.transform.position, Quaternion.identity);
+    }
+
+    private void DamageHealth(float health)
+    {
+        if(currentHealth > 0 )
+        {
+            currentHealth -= health;
+        }
+        else
+        {
+            currentHealth = 0;
+            Destroy(gameObject);
+        }
+    }
+
+}
